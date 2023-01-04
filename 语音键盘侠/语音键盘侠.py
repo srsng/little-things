252ced83e48343c8b86fea760b83e98710d7c42f
srsng <113006793+srsng@users.noreply.github.com>
2023-01-04 15:05:07 +0800

    Update 语音键盘侠.py

--- 语音键盘侠/语音键盘侠.py
+++ 语音键盘侠/语音键盘侠.py
@@ -1,42 +1,34 @@
-import keyboard
 import threading
+import keyboard
 from playsound import playsound
-
-
-def get_press(x):
-    x = str(x)[14:-1]
-    if x[-1] == 'p':
-        pass
-    else:
-        x = x[:-5]
-        return x
 
 
 def press(x):
     """
-
     :param x: 上一次按压的键的提示信息，来自keyboard中的类型
-    :return:
     """
     global pressed
-    global module
     global hot_words
     global sound_path
-    last_press = get_press(x)
+    global module
+    last_press = ''
 
-    if last_press is not None:
-        pressed.append(last_press)
-        length = len(hot_words)
-        for k in range(length):
-            if last_press == hot_words[k][-1] and pressed[-len(hot_words[k]):-1] == list(hot_words[k][:-1]):
-                threading.Thread(target=playsound, args=(sound_path[k],)).start()
+    def get_press(pressed):
+        nonlocal last_press
+        nonlocal x
+        x = str(x)[14:-1]
+        if x[-1] == 'n':
+            last_press = x[:-5]
+            pressed.append(last_press)
+
+    get_press(pressed)
+
+    for k in range(len(hot_words)):
+        if last_press == hot_words[k][-1] and pressed[-len(hot_words[k]):-1] == list(hot_words[k][:-1]):
+            threading.Thread(target=playsound, args=(sound_path[k],)).start()
 
 
-if __name__ == "__main__":
-    module = False
-    hot_words = []
-    sound_path = []
-    pressed = []
+def load_cmds(hot_words,sound_path):
     with open(r"load_cmds.txt", 'r', encoding='utf-8') as f:
         ls = f.readlines()
     cnt = 0
@@ -49,5 +41,12 @@ if __name__ == "__main__":
             sound_path.append(i)
             cnt += 1
 
+
+if __name__ == "__main__":
+    module = False
+    hot_words = []
+    sound_path = []
+    pressed = []
+    load_cmds(hot_words,sound_path)
     keyboard.hook(press)
     keyboard.wait()
