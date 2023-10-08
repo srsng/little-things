12da86e4641830643bcf9182029c128fc209203d
srsnng <113006793+srsng@users.noreply.github.com>
2023-10-08 15:56:54 +0800

    Update main.py

--- AutoRewards/main.py
+++ AutoRewards/main.py
@@ -2,10 +2,9 @@ from RewardSearch import DailySearch
 
 
 if __name__ == "__main__":
-    headless = True  # 想要看不见浏览器就不变，想看得见就改成False
     user_list = ["Deafult"] # 按readme.md修改这行
     for user in user_list:
-        ds = DailySearch(headless, user=user)
+        ds = DailySearch(False, user=user)
         ds.run()
         del ds
 
