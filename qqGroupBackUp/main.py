751e6e9230ff93e779e6569eb68e366ddf4b97cd
srsnng <113006793+srsng@users.noreply.github.com>
2023-10-13 22:10:43 +0800

    Update main.py

--- qqGroupBackUp/main.py
+++ qqGroupBackUp/main.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     groups = bc.get_group_numbers(save_to_txt=True)
 
     # 保存first_group_number的群基本资料与所有成员的基本资料
-    first_group_number = groups[0]
+    first_group_number = groups[0][0]
     bc.get_group_members(first_group_number, save_to_txt=True)
